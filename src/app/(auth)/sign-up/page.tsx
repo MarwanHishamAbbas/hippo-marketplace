4a5cbaa7413c5b7c84bb33d62a70cbd2285a3161
Marwan Hisham <marwanhiisham@outlook.com>
2024-02-29 00:01:54 +0200

    done with the sign up

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,113 @@
+"use client"
+
+import { buttonVariants } from "@/components/ui/Button"
+import { Icons } from "@/constants/Icons"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
+import { FC } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  AuthCredentialsValidator,
+  TAuthCredentialsValidator,
+} from "@/lib/validators/account-credentials-validator"
+import { Button } from "@/components/ui/Button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/Form"
+import { Input } from "@/components/ui/Input"
+import { cn } from "@/lib/utils"
+
+interface PageProps {}
+
+const Page: FC<PageProps> = ({}) => {
+  const form = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+    console.log(email, password)
+    // Send Data to server using TRPC
+  }
+
+  return (
+    <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col items-center space-y-2 text-center">
+          <Icons.logo className="h-20 w-20" />
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Create an account
+          </h1>
+          <Link
+            className={buttonVariants({
+              variant: "link",
+              className: "gap-1.5",
+            })}
+            href="/sign-in"
+          >
+            Already have an account? Sign-in
+            <ArrowRight className="h-4 w-4" />
+          </Link>
+        </div>
+        <div className="grid gap-6">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={cn({
+                          "focus-visible:ring-red-500":
+                            form.formState.errors.email,
+                        })}
+                        placeholder="you@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={cn({
+                          "focus-visible:ring-red-500":
+                            form.formState.errors.password,
+                        })}
+                        placeholder="Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Sign Up</Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Page
